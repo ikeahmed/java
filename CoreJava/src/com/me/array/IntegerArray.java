3ea4d68d4d39073ce969d3e1484e67c037d6024f
Ikhlas Ahmed <ike.ahmed@aa.com>
2021-04-20 08:04:58 -0500

    to give for programming exercies

---
+++ CoreJava/src/com/me/array/IntegerArray.java
@@ -0,0 +1,10 @@
+package com.me.array;
+
+public class IntegerArray {
+    public static void main(String[] args) {
+        int[] arr = new int[]{ 1,2,3,4,5,6,7,8,9,10 };
+        for (int i=0; i< arr.length; i++) {
+            System.out.println("Element at index " + i + " : "+ arr[i]);
+        }
+    }
+}
